86b7ac5baddf9879693a4bcc5909607429e2930f
David Cardenas <davidcardenas1993@yahoo.com>
2019-11-24 10:23:22 -0700

    rearranged firebase to js file including quick console.log to ensure js operation link

---
+++ js/app.js
@@ -0,0 +1,16 @@
+//console log to ensure operation of js link
+console.log("hello");
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDLIkLqrzyWYYaqgJpexa-LxI_5CpzuXWo",
+    authDomain: "train-scheduler-4fd71.firebaseapp.com",
+    databaseURL: "https://train-scheduler-4fd71.firebaseio.com",
+    projectId: "train-scheduler-4fd71",
+    storageBucket: "train-scheduler-4fd71.appspot.com",
+    messagingSenderId: "888050210280",
+    appId: "1:888050210280:web:6b0389f4857b059ed2405d",
+    measurementId: "G-2QWN5NHC28"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
